c2a7200a095e6260819afa7507b6f0d172deb7ef
unknowida <csa.devomax@gmail.com>
2022-03-09 18:08:00 +0100

    maj sortmedia title

--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -2,32 +2,32 @@
 
 async function getPhotographerAndMedia(photographerId) {
   // 1 fois pour reccupération de la réponse
-  const response = await fetch('./data/photographers.json')
+  const response = await fetch("./data/photographers.json");
   // 2 eme fois pour la requete transforme en JSON
-  const photographersAndMediaJson = await response.json()
+  const photographersAndMediaJson = await response.json();
 
-  const photographers = photographersAndMediaJson.photographers
+  const photographers = photographersAndMediaJson.photographers;
 
-  let currentPhotographer
+  let currentPhotographer;
   // Boucle qui va s'itérer et permettra de vérifier reccuperer le photographe qui nous interesse de tout les photographers que l'on aura placer en paramettre.
   for (let photographer of photographers) {
     if (photographer.id === photographerId) {
-      currentPhotographer = photographer
+      currentPhotographer = photographer;
     }
   }
 
-  const allMedias = photographersAndMediaJson.media
+  const allMedias = photographersAndMediaJson.media;
 
-  let currentPhotographerMedias = []
+  let currentPhotographerMedias = [];
   // l'iteration se fait sur tout les médias , si un media possede le même photographerId demandé par le
   for (let media of allMedias) {
     if (media.photographerId === photographerId) {
       // push permet d'ajouter l'arguùment (media) en paramettre du tableau --> ex: (let currentPhotographerMedias = [media])
-      currentPhotographerMedias.push(media)
+      currentPhotographerMedias.push(media);
     }
   }
 
-  return [currentPhotographer, currentPhotographerMedias]
+  return [currentPhotographer, currentPhotographerMedias];
 
   // il s'agit de réccuperer uniquement le tableau ["media"] du fichier JSON
   // return photographersAndMediaJson.media
@@ -63,47 +63,46 @@ async function loadPhotographerData() {
 
   // Depuis la page photographer.html , je reccupere l'id du du photographe "photographId" depuis l'URL (location.seearch) ex:"?photographerId=243"
   //? pourquoi mettre new pour cette variable?
-  const searchParams = new URLSearchParams(location.search)
+  const searchParams = new URLSearchParams(location.search);
   // le parseInt permettra de convertir et spécifier que 'photographerId' sera un nombre
-  const photographerId = parseInt(searchParams.get('photographerId'))
+  const photographerId = parseInt(searchParams.get("photographerId"));
 
   // Récupère les datas des photographes et des medias du fichier JSON
   // le role de la fonction getPhotographerAndMedia est de me retourner les photographes et les medias par rapport à l'Id que je lui communique en argument (photographerId)
   // ?Comment s'appelle la technique de const [] ci-sessous
-  const [photographer, medias] = await getPhotographerAndMedia(photographerId)
+  const [photographer, medias] = await getPhotographerAndMedia(photographerId);
 
-  return [photographer, medias]
+  return [photographer, medias];
 }
 
 async function reloadMedia(medias) {
-  let index = 0
+  let index = 0;
   for (const media of medias) {
-    const mediaElement = document.querySelector(`[data-id='${media.id}']`)
-    mediaElement.style.order = index
-    index += 1
+    const mediaElement = document.querySelector(`[data-id='${media.id}']`);
+    mediaElement.style.order = index;
+    index += 1;
   }
 }
 
-async function loadAndDisplayMedia(photographer, media) {
-  const mainSectionElement = document.querySelector('#main')
+async function loadAndDisplayMedia(photographer, medias) {
+  const mainSectionElement = document.querySelector("#main");
 
-  const responsePhotographer = photographerDetailsFactory(photographer)
+  const responsePhotographer = photographerDetailsFactory(photographer);
   mainSectionElement.appendChild(
-    responsePhotographer.createPhotographersDetailsDOM(),
-  )
-}
+    responsePhotographer.createPhotographersDetailsDOM()
+  );
 
-//changer les proprietés des order elements
+  //changer les proprietés des order elements
 
-const mediaSectionElement = document.createElement('section')
-mediaSectionElement.className = 'media-section'
-mainSectionElement.appendChild(mediaSectionElement)
-// Création d'une boucle "for(const...of...){} qui va lire et associer dans l'ordre la variable "photograph" pour chaque accolade (ou tableau objet) du fichier JSON du la clé ["photographers"]
-for (const media of medias) {
-  const response = mediaFactory(media)
-  mediaSectionElement.appendChild(response.createCardByPhotographerDOM())
-  // debugger
-}
+  const mediaSectionElement = document.createElement("section");
+  mediaSectionElement.className = "media-section";
+  mainSectionElement.appendChild(mediaSectionElement);
+  // Création d'une boucle "for(const...of...){} qui va lire et associer dans l'ordre la variable "photograph" pour chaque accolade (ou tableau objet) du fichier JSON du la clé ["photographers"]
+  for (const media of medias) {
+    const response = mediaFactory(media);
+    mediaSectionElement.appendChild(response.createCardByPhotographerDOM());
+    // debugger
+  }
 }
 
 // displayData(photographers);
@@ -113,35 +112,66 @@ for (const media of medias) {
 const registerSortByLikesClickListener = (medias) => {
   const sortByLikesOnClickCallback = () => {
     // debugger
-    const sortedMedias = [...medias]
+    const sortedMedias = [...medias];
     sortedMedias.sort((media1, media2) => {
-      return media2.likes - media1.likes
-    })
-    reloadMedia(sortedMedias)
-  }
+      return media2.likes - media1.likes;
+    });
+    reloadMedia(sortedMedias);
+  };
 
   document
-    .querySelector('.sort-by-likes')
-    .addEventListener('click', sortByLikesOnClickCallback)
-}
+    .querySelector(".sort-by-likes")
+    .addEventListener("click", sortByLikesOnClickCallback);
+};
 
 const registerSortByDateListener = (medias) => {
   const sortByDateOnClickCallback = () => {
-    const sortedMedias = [...medias]
+    const sortedMedias = [...medias];
     sortedMedias.sort((media1, media2) => {
-      return new Date(media2.date) < new Date(media1.date)
-    })
-    reloadMedia(sortedMedias)
-  }
+      const date1 = new Date(media1.date);
+      const date2 = new Date(media2.date);
+
+      return date2.getTime() - date1.getTime
+    });
+    reloadMedia(sortedMedias);
+  };
 
   document
-    .querySelector('.sort-by-date')
-    .addEventListener('click', sortByDateOnClickCallback)
-}
+    .querySelector(".sort-by-date")
+    .addEventListener("click", sortByDateOnClickCallback);
+};
+
+const registerSortByTitleListener = (medias) => {
+  const sortByTitleOnClickCallback = () => {
+    const sortedMedias = [...medias];
+    sortedMedias.sort((media1, media2) => {
+      const title1 = media1.title;
+      const title2 = media2.title;
+
+      // Si le titre n'est pas défini alors on le met à la fin du tableau
+      if (!title1) {
+        return 1;
+      }
+
+      if (!title2) {
+        return -1;
+      }
+
+      return title1.localeCompare(title2);
+    });
+    reloadMedia(sortedMedias);
+  };
+
+  document
+    .querySelector(".sort-by-title")
+    .addEventListener("click", sortByTitleOnClickCallback);
+};
 
 loadPhotographerData().then(([photographer, medias]) => {
-  registerSortByLikesClickListener(medias)
+  registerSortByLikesClickListener(medias);
+  registerSortByDateListener(medias);
+  registerSortByTitleListener(medias);
 
   // Page d'acceuil vide aprèes chargement HTML/CSS , ensuite on fait loa+dAndDisplayPhotographers() pour amorcer toutes les étapes contenues dans la (function loadAndDisplayPhotographers())
-  loadAndDisplayMedia(photographer, medias)
-})
+  loadAndDisplayMedia(photographer, medias);
+});
